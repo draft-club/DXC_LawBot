00ca79cd880cff8161066f24407634ee5777fbca
draft-club <hicker.coder.2025@gmail.com>
2024-10-28 15:10:07 +0100

    local rag set up with chroma and langchain

---
+++ local_legalbot_RAG_langchain/main.py
@@ -0,0 +1,23 @@
+
+
+def main():
+
+    # Check if the database should be cleared (using the --clear flag).
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+    if args.reset:
+        print("✨ Clearing Database")
+        clear_database()
+
+    # Create (or update) the data store.
+    documents = load_documents()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+    query_rag(query_text)
---
+++ local_legalbot_RAG_langchain/prompts.py
@@ -0,0 +1,10 @@
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+

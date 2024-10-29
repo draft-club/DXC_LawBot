7bdd3579ce238a4719602523a82b12d150c6a84e
draft-club <hicker.coder.2025@gmail.com>
2024-10-29 11:46:06 +0100

    change opesource embedding model

--- local_legalbot_RAG_langchain/embedding.py
+++ local_legalbot_RAG_langchain/embedding.py
@@ -1,24 +1,12 @@
 from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_aws import BedrockEmbeddings
 
 
-def get_embedding_function(embedding_type="bedrock"):
+def get_embedding_function():
     """
-    Returns the specified embedding function.
-
-    Args:
-        embedding_type (str): The type of embedding to use ("bedrock" or "ollama").
+    Returns the Ollama embedding function.
 
     Returns:
-        embeddings: The embedding function object.
+        embeddings: The Ollama embedding function object.
     """
-    if embedding_type == "bedrock":
-        embeddings = BedrockEmbeddings(
-            credentials_profile_name="default", region_name="us-east-1"
-        )
-    elif embedding_type == "ollama":
-        embeddings = OllamaEmbeddings(model="nomic-embed-text")
-    else:
-        raise ValueError("Invalid embedding type. Choose 'bedrock' or 'ollama'.")
-
+    embeddings = OllamaEmbeddings(model="mxbai-embed-large") #opensource embedding Model
     return embeddings

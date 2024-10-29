3c968dbfc3fa27efca437d1737f588192ab3c574
draft-club <hicker.coder.2025@gmail.com>
2024-10-29 11:06:49 +0100

    langchain_aws + chroma fix

--- local_legalbot_RAG_langchain/embedding.py
+++ local_legalbot_RAG_langchain/embedding.py
@@ -1,10 +1,24 @@
 from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_community.embeddings.bedrock import BedrockEmbeddings
+from langchain_aws import BedrockEmbeddings
 
 
-def get_embedding_function():
-    embeddings = BedrockEmbeddings(
-        credentials_profile_name="default", region_name="us-east-1"
-    )
-    # embeddings = OllamaEmbeddings(model="nomic-embed-text")
+def get_embedding_function(embedding_type="bedrock"):
+    """
+    Returns the specified embedding function.
+
+    Args:
+        embedding_type (str): The type of embedding to use ("bedrock" or "ollama").
+
+    Returns:
+        embeddings: The embedding function object.
+    """
+    if embedding_type == "bedrock":
+        embeddings = BedrockEmbeddings(
+            credentials_profile_name="default", region_name="us-east-1"
+        )
+    elif embedding_type == "ollama":
+        embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    else:
+        raise ValueError("Invalid embedding type. Choose 'bedrock' or 'ollama'.")
+
     return embeddings
